3a0abd4409bef81c206cd9a6dec4890a3817df21
Mahmoud Dana <mahmoudd3na@hotmail.com>
2023-07-05 22:31:15 +0300

    dfdf

---
+++ Java Script/1.1_primitive_data_types.js
@@ -0,0 +1,11 @@
+let message = "you enter a dimly lit room"
+let treasureCount =33; 
+let mysteriousArtifcate= undefined;
+var key = {
+    material: 'gold',
+    carvings: 'intricate',
+    color: 'golden'
+  };
+  let secretpassage =null;  
+let isTrapActivated= true; 
+console.log(mysteriousArtifcate); 
